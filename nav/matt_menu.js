f9234736e7b7372ad6c63694ff535bfb3ddabeaf
Jonathan McDill <oleosjo@gmail.com>
2025-07-03 15:57:20 -0400

    first commit

---
+++ nav/matt_menu.js
@@ -0,0 +1,189 @@
+function cdd_menu527504(){//////////////////////////Start Menu Data/////////////////////////////////
+
+//**** NavStudio, (c) 2004, OpenCube Inc.,  -  www.opencube.com ****
+//Note: This data file may be manually customized outside of the visual interface.
+
+    //Unique Menu Id
+	this.uid = 527504
+
+
+/**********************************************************************************************
+
+                               Icon Images
+
+**********************************************************************************************/
+
+
+    //Inline positioned icon images (flow with the item text)
+
+	this.rel_icon_image0 = "matt1.GIF"
+	this.rel_icon_rollover0 = "matt1.GIF"
+	this.rel_icon_image_wh0 = "9,9"
+
+
+
+
+/**********************************************************************************************
+
+                              Global - Menu Container Settings
+
+**********************************************************************************************/
+
+
+	this.menu_background_color = "#dee7ed"
+	this.menu_border_color = "#000000"
+	this.menu_border_width = 1
+	this.menu_padding = "4,4,4,4"
+	this.menu_border_style = "solid"
+	this.divider_caps = false
+	this.divider_width = 3
+	this.divider_height = 3
+	this.divider_background_color = "#dee7ed"
+	this.divider_border_style = "none"
+	this.divider_border_width = 0
+	this.divider_border_color = "#000000"
+	this.menu_is_horizontal = false
+	this.menu_width = "120"
+	this.menu_xy = "-80,-2"
+
+
+
+
+/**********************************************************************************************
+
+                              Global - Menu Item Settings
+
+**********************************************************************************************/
+
+
+	this.icon_rel = 0
+	this.menu_items_background_color_roll = "#ecede7"
+	this.menu_items_text_color = "#333333"
+	this.menu_items_text_decoration = "none"
+	this.menu_items_font_family = "Arial"
+	this.menu_items_font_size = "11px"
+	this.menu_items_font_style = "normal"
+	this.menu_items_font_weight = "normal"
+	this.menu_items_text_align = "left"
+	this.menu_items_padding = "4,5,4,5"
+	this.menu_items_border_style = "solid"
+	this.menu_items_border_color = "#a48a86"
+	this.menu_items_border_width = "1"
+	this.menu_items_width = 90
+	this.menu_items_text_decoration_roll = "none"
+	this.menu_items_border_color_roll = "#004080"
+	this.menu_items_text_color_roll = "#000040"
+	this.menu_items_font_family_roll = "Arial"
+	this.menu_items_font_style_roll = "normal"
+	this.menu_items_font_weight_roll = "normal"
+	this.menu_items_font_size_roll = "11px"
+	this.menu_items_background_color = "#ecede7"
+
+
+
+
+/**********************************************************************************************
+
+                              Main Menu Settings
+
+**********************************************************************************************/
+
+
+        this.menu_items_width_main = 100
+        this.menu_is_horizontal_main = true
+
+        this.item0 = " Blog"
+        this.item1 = " Essays & Articles "
+        this.item2 = " Family"
+        this.item3 = " About"
+        this.item4 = " Resolved"
+        this.item5 = " Keeping Families Together "
+
+        this.item_width0 = 70
+        this.item_width1 = 132
+        this.item_width2 = 68
+        this.item_width3 = 72
+        this.item_width4 = 82
+        this.item_width5 = 159
+
+        this.url0 = "http://waynemcdill.net/blog.html"
+        this.status0 = "Matthew's Blog"
+        this.url1 = "http://waynemcdill.net/documents.html"
+        this.status1 = "Essays & Articles "
+        this.url2 = "http://waynemcdill.net/family.html"
+        this.status2 = "Family"
+        this.url3 = "http://waynemcdill.net/about.html"
+        this.status3 = "About Matthew"
+        this.url4 = "http://waynemcdill.net/resolved.html"
+        this.status4 = "Resolved"
+        this.url5 = "http://waynemcdill.net/kft.html"
+        this.status5 = "Keeping Families Together "
+
+
+
+
+/**********************************************************************************************
+
+                              Sub Menu Settings
+
+**********************************************************************************************/
+
+
+    //Sub Menu 0
+
+        this.menu_items_text_decoration_roll0 = "none"
+        this.menu_xy0 = "-68,-1"
+
+        this.item0_0 = "Archives"
+
+        this.icon_rel0_0 = -1
+        this.icon_abs0_0 = -1
+
+        this.url0_0 = "http://waynemcdill.net/archives.html"
+
+
+
+    //Sub Menu 1
+
+
+
+
+    //Sub Menu 2
+
+
+
+
+    //Sub Menu 3
+
+
+
+
+    //Sub Menu 4
+
+
+
+
+    //Sub Menu 5
+
+
+
+
+}///////////////////////// END Menu Data /////////////////////////////////////////
+
+
+
+//Document Level Settings
+
+cdd__activate_onclick = false
+cdd__showhide_delay = 100
+cdd__url_target = "main"
+cdd__url_features = ""
+cdd__display_urls_in_status_bar = true
+cdd__default_cursor = "hand"
+
+
+//NavStudio Code (Warning: Do Not Alter!)
+
+if (window.showHelp){b_type = "ie"; if (!window.attachEvent) b_type += "mac";}if (document.createElementNS) b_type = "dom";if (navigator.userAgent.indexOf("afari")>-1) b_type = "safari";if (window.opera) b_type = "opera"; qmap1 = "\<\script language=\"JavaScript\" vqptag='loader_sub' src=\""; qmap2 = ".js\">\<\/script\>";;function iesf(){};;function vqp_error(val){alert(val)}
+if (b_type){document.write(qmap1+cdd__codebase+"pbrowser_"+b_type+qmap2);document.close();}
+
---
+++ nav/menu.js
@@ -0,0 +1,219 @@
+function cdd_menu870810(){//////////////////////////Start Menu Data/////////////////////////////////
+
+//**** NavStudio, (c) 2004, OpenCube Inc.,  -  www.opencube.com ****
+//Note: This data file may be manually customized outside of the visual interface.
+
+    //Unique Menu Id
+	this.uid = 870810
+
+
+/**********************************************************************************************
+
+                               Icon Images
+
+**********************************************************************************************/
+
+
+    //Inline positioned icon images (flow with the item text)
+
+	this.rel_icon_image0 = "squares_0.gif"
+	this.rel_icon_rollover0 = "squares_0_hl.gif"
+	this.rel_icon_image_wh0 = "13,8"
+
+	this.rel_icon_image1 = "arrows_6.gif"
+	this.rel_icon_rollover1 = "arrows_6_hl.gif"
+	this.rel_icon_image_wh1 = "10,8"
+
+
+
+
+/**********************************************************************************************
+
+                              Global - Menu Container Settings
+
+**********************************************************************************************/
+
+
+	this.menu_background_color = "#CC9966"
+	this.menu_border_color = "#582c2c"
+	this.menu_border_width = 1
+	this.menu_padding = "2,4,2,4"
+	this.menu_border_style = "solid"
+	this.divider_caps = false
+	this.divider_width = 0
+	this.divider_height = 1
+	this.divider_background_color = "#996633"
+	this.divider_border_style = "none"
+	this.divider_border_width = 0
+	this.divider_border_color = "#000000"
+	this.menu_is_horizontal = false
+	this.menu_width = "120"
+	this.menu_xy = "-14,-19"
+	this.menu_scroll_direction = 4
+	this.menu_scroll_reverse_on_hide = true
+	this.menu_scroll_delay = 0
+	this.menu_scroll_step = 5
+	this.menu_animation = "progid:DXImageTransform.Microsoft.none(duration=0.4)"
+
+
+
+
+/**********************************************************************************************
+
+                              Global - Menu Item Settings
+
+**********************************************************************************************/
+
+
+	this.icon_rel = 1
+	this.menu_items_background_color_roll = "#996633"
+	this.menu_items_text_color = "#754839"
+	this.menu_items_text_decoration = "none"
+	this.menu_items_font_family = "Georgia"
+	this.menu_items_font_size = "11px"
+	this.menu_items_font_style = "normal"
+	this.menu_items_font_weight = "normal"
+	this.menu_items_text_align = "left"
+	this.menu_items_padding = "4,5,4,5"
+	this.menu_items_border_style = "solid"
+	this.menu_items_border_width = 0
+	this.menu_items_width = 110
+	this.menu_items_text_color_roll = "#FFCC99"
+
+
+
+
+/**********************************************************************************************
+
+                              Main Menu Settings
+
+**********************************************************************************************/
+
+
+        this.menu_background_color_main = "#996633"
+        this.menu_items_background_color_main = "#996633"
+        this.menu_items_background_color_roll_main = "#CC9966"
+        this.menu_items_text_color_main = "#FFCC99"
+        this.menu_items_text_color_roll_main = "#996633"
+        this.menu_border_color_main = "#666633"
+        this.menu_items_font_family_main = "Georgia"
+        this.menu_items_font_weight_main = "normal"
+        this.menu_items_font_style_main = "normal"
+        this.menu_items_font_size_main = "11px"
+        this.menu_items_border_color_main = "#FF6600"
+        this.menu_items_border_color_roll_main = "#FF6600"
+        this.menu_width_main = 200
+        this.menu_is_horizontal_main = false
+        this.divider_background_color_main = "#663333"
+
+        this.item0 = " Home"
+        this.item1 = " Biography"
+        this.item2 = " Articles"
+        this.item3 = " Books"
+        //this.item4 = " Board"
+        //this.item5 = " Weblog"
+        this.item4 = " Southeastern"
+        this.item5 = " Links"
+
+        this.icon_rel3 = 1
+
+        this.url0 = "../home.html"
+        this.url1 = "../bio.html"
+        this.url3 = "../books.html"
+        //this.url4 = "../board.html"
+        //this.url5 = "../blog.html"
+        this.url4 = "../se.html"
+        this.url5 = "../links.html"
+
+
+
+
+/**********************************************************************************************
+
+                              Sub Menu Settings
+
+**********************************************************************************************/
+
+
+    //Sub Menu 0
+
+
+
+
+    //Sub Menu 1
+
+
+
+
+    //Sub Menu 2
+
+        this.menu_items_font_family2 = "Georgia"
+        this.menu_items_font_weight2 = "normal"
+        this.menu_items_font_style2 = "normal"
+        this.menu_items_font_size2 = "11px"
+        this.menu_width2 = 147
+
+        this.item2_0 = "Preaching"
+        this.item2_1 = "Pastoral Leadership"
+        this.item2_2 = "Church Planting"
+        this.item2_3 = "Evangelism"
+        this.item2_4 = "Miscellanious"
+
+        this.icon_rel2_0 = -1
+        this.icon_rel2_1 = -1
+        this.icon_rel2_2 = -1
+        this.icon_rel2_3 = -1
+        this.icon_rel2_4 = -1
+
+        this.url2_0 = "../articles/preaching.html"
+        this.url2_1 = "../articles/pastoral-leadership.html"
+        this.url2_2 = "../articles/church-planting.html"
+        this.url2_3 = "../articles/evangelism.html"
+        this.url2_4 = "../articles/misc.html"
+
+
+
+    //Sub Menu 3
+
+
+
+
+    //Sub Menu 4
+
+
+
+
+    //Sub Menu 5
+
+
+
+
+    //Sub Menu 6
+
+
+
+
+    //Sub Menu 7
+
+
+
+
+}///////////////////////// END Menu Data /////////////////////////////////////////
+
+
+
+//Document Level Settings
+
+cdd__activate_onclick = false
+cdd__showhide_delay = 100
+cdd__url_target = "main"
+cdd__url_features = ""
+cdd__display_urls_in_status_bar = true
+cdd__default_cursor = "hand"
+
+
+//NavStudio Code (Warning: Do Not Alter!)
+
+if (window.showHelp){b_type = "ie"; if (!window.attachEvent) b_type += "mac";}if (document.createElementNS) b_type = "dom";if (navigator.userAgent.indexOf("afari")>-1) b_type = "safari";if (window.opera) b_type = "opera"; qmap1 = "\<\script language=\"JavaScript\" vqptag='loader_sub' src=\""; qmap2 = ".js\">\<\/script\>";;function iesf(){};;function vqp_error(val){alert(val)}
+if (b_type){document.write(qmap1+cdd__codebase+"pbrowser_"+b_type+qmap2);document.close();}
+
